57e7463e483ca6df4ebaee72c24c10d51c4e0fed
whoisrmzn <rmuxtarov246@gmail.com>
2022-07-06 01:45:59 +0400

    29 web

--- Student/src/module-info.java
+++
@@ -1,5 +0,0 @@
-module Student {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
---
+++ springmvc/src/main/java/controller/BookController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+	
+	@GetMapping("/books")
+	public String showBooksPage() {
+		return "books";
+	}
+	
+	@GetMapping("/cars")
+	public String showCarsPage() {
+		return "cars";
+	}
+	
+
+}
